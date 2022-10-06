fa2dc680d25645f4b9fd42a34c44401077e501fa
kabradshaw1 <itllbok82@gmail.com>
2022-10-05 23:40:01 -0400

    first commit

---
+++ E-commerce/ecommerce/README.md
@@ -0,0 +1,4 @@
+# Django-Rest-MySQL-Jquery 
+
+## Purpose
+This project is entended to practice using some tools I'm only beginning to mess around with.  I've not actually used rest yet at all.
---
+++ E-commerce/ecommerce/store/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, REDIRECT
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
+# Create your views here.
